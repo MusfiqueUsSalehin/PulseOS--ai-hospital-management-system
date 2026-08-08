e26cf142733b7e9af9827bbb2e2b7d86722094d2
MusfiqueUsSalehin <musfiqueishti@gmail.com>
2026-08-09 05:40:52 +0600

    Betterauth setup

--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -1,12 +1,24 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import type { Role, Statements } from "better-auth/plugins";
+import { admin } from "better-auth/plugins/admin";
 import { MongoClient } from "mongodb";
 
-const client = new MongoClient(process.env.DATABASE_URL!);
+const client = new MongoClient(process.env.MONGO_URI||"");
 const db = client.db();
 
 export const auth = betterAuth({
   database: mongodbAdapter(db),
   baseURL: process.env.BETTER_AUTH_URL || "http://localhost:5000/",
+  trustedOrigins: [process.env.FRONTEND_URL || "http://localhost:5173"],
   emailAndPassword: { enabled: true },
+  plugins: [
+    admin({
+      defaultRole: "patient",
+      adminRole: ["admin", "superadmin"],
+    
+    }
+  )]
+    
+  
 });
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,4 @@
-import dotenv from "dotenv";
+import "dotenv/config";
 import express, {
     type Application,
     type Request,
@@ -14,9 +14,13 @@ import cookieParser from "cookie-parser";
 
 import morgan from "morgan";
 import { connectDB } from "./config/db";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+import { fromNodeHeaders } from "better-auth/node";
 
-// Load environment variables from .env file
-dotenv.config();
+
+
+
 
 
 console.log("FRONTEND_URL from env:", process.env.PORT);
@@ -28,8 +32,13 @@ const PORT: number = Number(process.env.PORT) || 5000;
 
 
 // Middleware to parse incoming JSON requests
-app.use(cors({origin: process.env.FRONTEND_URL || "http://localhost:5173"}
+app.use(cors(
+    {
+        origin: process.env.FRONTEND_URL || "http://localhost:5173",
+        credentials: true
 
+    }
+    
 ));
 
 
@@ -59,6 +68,19 @@ if (process.env.NODE_ENV === "development") {
 //basic route for testing the server
 app.get("/", (req: Request, res: Response) => {
     res.send("Server is running");
+});
+
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
+
+
+
+app.get("/api/me", async (req, res) => {
+ 	const session = await auth.api.getSession({
+      headers: fromNodeHeaders(req.headers),
+    });
+	return res.json(session);
 });
 
 
