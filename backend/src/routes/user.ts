ab66119842ea40780eb7ab8a0f3029a46f6f18d1
MusfiqueUsSalehin <musfiqueishti@gmail.com>
2026-09-02 05:25:17 +0600

    update user and checkrole middleware created

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -4,11 +4,12 @@ const userRouter = express.Router();
 
 import { getUserbyId, updateUser } from "../controllers/user";
 import { requireAuth } from "../middleware/auth";
+import { checkRole } from "../middleware/checkRole";
 
 userRouter.get("/:id", requireAuth, getUserbyId);
 
 //only admin and medical staff can update user details.
-userRouter.put("/update/:id", requireAuth, updateUser);
+userRouter.put("/update/:id", requireAuth, checkRole(["admin", "doctor", "nurse"]), updateUser);
 
 
 export default userRouter;
